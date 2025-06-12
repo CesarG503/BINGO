6f8f426ea11a5ec14292250a62a368331c2b6c34
CesarG503 <cesargaray06@gmail.com>
2025-06-12 16:34:56 -0600

    funcionamiento de la tienda y roles

--- js/db/db.js
+++ js/db/db.js
@@ -7,16 +7,16 @@ const pool = new Pool({
 
   //conectar de manera local
 
-  host: process.env.LOCAL,
-  user: 'postgres',
-  password: 'admin', // Ingresa tu contraseña de PostgreSQL (cambiala loco)
-  database: 'BingoDB',
-  port: 5432
+ // host: process.env.LOCAL,
+  //user: 'postgres',
+  //password: 'admin', // Ingresa tu contraseña de PostgreSQL (cambiala loco)
+  //database: 'BingoDB',
+  //port: 5432
   
-  //connectionString: process.env.DATA_BASE_URL,
-  //ssl: {
-  //  rejectUnauthorized: false, // << ESTA LÍNEA SOLUCIONA EL ERROR
-  //},
+  connectionString: process.env.DATA_BASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // << ESTA LÍNEA SOLUCIONA EL ERROR
+  },
 });
 
 pool.connect((err) => {
