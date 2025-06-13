b574c00103ea2cf96d3a2162dacc4927dbea9eaa
Mixgyt <gmix990@gmail.com>
2025-06-12 21:58:36 -0600

    Creacion de base de los home

--- public/js/admin/admin_home.js
+++ public/js/admin/admin_home.js
@@ -1,9 +1,31 @@
-import { API_BASE_URL } from "../util/base_url";
 
 const form_create_room = document.getElementById("create-room-form");
 
 form_create_room.addEventListener("submit", createRoom);
 
 function createRoom(e){
-    console.log(localStorage.getItem('userId'))
+    e.preventDefault();
+    console.log(localStorage.getItem('token'));
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  autenticarRol()
+})
+
+function autenticarRol() {
+    const token = localStorage.getItem("token")
+    if (!token) {
+        window.location.href = "login.html"
+        return
+    }
+    try {
+        const rol_usuario = JSON.parse(atob(token.split('.')[1]))
+        if (rol_usuario.rol !== 0) {
+            window.location.href = "index.html"
+            return
+        }
+    } catch (e) {
+        window.location.href = "login.html"
+        return
+    }
 }
