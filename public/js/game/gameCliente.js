cf021f760268f768cb1c69ad2d345b6daa45ede8
Juan Sandoval <js3729395@gmail.com>
2025-06-17 17:16:50 -0600

    Mejoras en el diseño y estructura del juego Bingo

--- public/js/game/gameCliente.js
+++ public/js/game/gameCliente.js
@@ -1,44 +1,53 @@
-import {slide} from "./slide.js";
+import { slide } from "./slide.js";
 // const wrapper = document.getElementById("wrapper");
-let posicion = 0
-const cardList = document.getElementById("cardList");
-const bolasSeleccionadas = []; //bolas seleccionadas por el usuario
+let posicion = 0;
+const cardList = document.getElementById("cardList"); //bolas seleccionadas por el usuario
 const fondo = [
-  "bg-primary-subtle",
-  "bg-success-subtle",
-  "bg-info-subtle",
-  "bg-warning-subtle",
-  "bg-danger-subtle",
+  "bg-primary",
+  "bg-success",
+  "bg-info",
+  "bg-warning",
+  "bg-danger",
 ];
+
+let bolasSeleccionadas = [3, 17, 48,32, 64, 70, 75, 1, 15, 29, 45, 60, 66, 72, 74];
+
+let diccionarioCartones = {
+  1: [],
+  2: [],
+  3: [],
+  4: [],
+  5: [],
+};
 
 let tarjeta = [
   [
-    [1, 2, 3, 4, 5],
-    [17, 16, 18, 19, 20],
-    [31, 32, "FREE", 34, 35],
-    [46, 47, 48, 49, 50],
-    [61, 62, 63, 64, 65],
+    [1, 16, 31, 46, 61],
+    [2, 17, 32, 47, 62],
+    [3, 18, "FREE", 48, 63],
+    [4, 19, 34, 49, 64],
+    [5, 20, 35, 50, 65],
   ],
   [
-    [1, 2, 3, 4, 5],
-    [17, 16, 18, 19, 20],
-    [31, 32, "FREE", 34, 35],
-    [46, 47, 48, 49, 50],
-    [61, 62, 63, 64, 65],
+    [6, 21, 36, 51, 66],
+    [7, 22, 37, 52, 67],
+    [8, 23, "FREE", 53, 68],
+    [9, 24, 39, 54, 69],
+    [10, 25, 40, 55, 70],
   ],
   [
-    [6, 7, 8, 9, 10],
-    [21, 22, 23, 24, 25],
-    [36, 37, "FREE", 39, 40],
-    [51, 52, 53, 54, 55],
-    [66, 67, 68, 69, 70],
+    [11, 26, 41, 56, 71],
+    [12, 27, 42, 57, 72],
+    [13, 28, "FREE", 58, 73],
+    [14, 29, 44, 59, 74],
+    [15, 30, 45, 60, 75],
   ],
   [
-    [6, 7, 8, 9, 10],
-    [21, 22, 23, 24, 25],
-    [36, 37, "FREE", 39, 40],
-    [51, 52, 53, 54, 55],
-    [66, 67, 68, 69, 70],
+    [1, 16, 31, 46, 61],
+    [2, 17, 32, 47, 62],
+    [3, 18, "FREE", 48, 63],
+    [4, 19, 34, 49, 64],
+    [5, 20, 35, 50, 65],
   ],
 ];
 
@@ -54,6 +63,7 @@ function renderCard(arreglo) {
   // contenedor general
   const container = document.createElement("div");
   container.className = "tablas-numeros text-center";
+  container.id = "carton-" + (posicion + 1);
 
   // título
   const h2 = document.createElement("h2");
@@ -74,10 +84,10 @@ function renderCard(arreglo) {
   const trHead = document.createElement("tr");
 
   const letras = ["b", "i", "n", "g", "o"];
-  
+
   for (let i = 0; i < letras.length; i++) {
     const th = document.createElement("th");
-    th.className =  fondo[i];
+    th.className = fondo[i];
     const div = document.createElement("div");
     div.className = "bola bola-" + letras[i];
 
@@ -96,10 +106,7 @@ function renderCard(arreglo) {
     const tr = document.createElement("tr");
     for (let j = 0; j < arreglo[i].length; j++) {
       const td = document.createElement("td");
-      const div = document.createElement("div");
-      div.className = "bola bola-g"; // puedes personalizar según columna
-      div.textContent = arreglo[i][j];
-      td.appendChild(div);
+      td.textContent = arreglo[i][j];
       tr.appendChild(td);
     }
     tbody.appendChild(tr);
@@ -112,7 +119,31 @@ function renderCard(arreglo) {
   cardList.appendChild(li);
 }
 
-function colorBolita(numero){
+// Aun no se como guardar las que ya fueron seleccionadas
+document.getElementById("cardList").addEventListener("click", (e) => {
+  const target = e.target;
+  console.log(target);
+
   
 
+  if (bolasSeleccionadas.includes(parseInt(target.innerText))) {
+    console.log("Se puede seleccionar la bola");
+    target.classList.add(fondo[asignarColor(parseInt(target.innerText))]);
+  } else {
+    console.log("Bola no seleccionable");
+  }
+});
+
+function asignarColor(numeroBola) {
+  if (numeroBola > 0 && numeroBola <= 15) {
+    return 0;
+  } else if (numeroBola > 15 && numeroBola <= 30) {
+    return 1;
+  } else if (numeroBola > 30 && numeroBola <= 45) {
+    return 2;
+  } else if (numeroBola > 45 && numeroBola <= 60) {
+    return 3;
+  } else if (numeroBola > 60 && numeroBola <= 75) {
+    return 4;
+  }
 }
